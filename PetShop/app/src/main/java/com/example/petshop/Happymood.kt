37e244481c66a70d6d0b811e6f5a5187c3521c4d
Fei324 <flu3@ualberta.ca>
2026-09-02 14:48:50 -0600

    Update Happymood.kt

--- PetShop/app/src/main/java/com/example/petshop/Happymood.kt
+++ PetShop/app/src/main/java/com/example/petshop/Happymood.kt
@@ -1,5 +1,5 @@
 package com.example.petshop
-
+// this section i get help with google gemini, "how to do abstract base class in Kotlin"
 class Happymood(date: String) : Mood(date) {
     override fun getMood(): String {
         return "Happy"
